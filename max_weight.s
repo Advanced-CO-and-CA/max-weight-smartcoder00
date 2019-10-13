26aa866d467ea7c2a033647ca6be0918dab35d2f
Jethin Sekhar R <jethin@qualcomm.com>
2019-10-13 20:47:10 +0530

    Adding More comments to Max Weight Implementation Added Address as output
    Signed-off-by: Jethin Sekhar R

--- max_weight.s
+++ max_weight.s
@@ -15,9 +15,9 @@
                     .word 0x256c8700                @(0010 0101 0110 1100 1000 0111 0000 0000 – 11)
     data_end:       .word 0x295468f2                @(0010 1001 0101 0100 0110 1000 1111 0010 – 14)
 
-    output_num:     .word 0
-    output_weight:  .word 0
-    output_address: .word 0
+    output_num:     .word 0                         @ Output Value
+    output_weight:  .word 0                         @ Output Weight
+    output_address: .word 0                         @ Output Address, An additional output
 
 @ text section
       .text
@@ -40,9 +40,9 @@ get_weight:                                         @ Calculate weight and store
     push {input_number, lr}
     mov  temp_weight, #0
     weight_loop:
-        lsls input_number, #1
-        adc  temp_weight, #0
-        bne  weight_loop
+        lsls input_number, #1                       @ Left Shift and collect the carry bit
+        adc  temp_weight, #0                        @ Increment temp_weight if carry
+        bne  weight_loop                            @ Continue if non zero
     pop  {input_number, pc}
 
 /*** Function: update the results ***************************************************************/
@@ -51,6 +51,7 @@ update_results:                                     @ Update the Results in resu
     push {lr}
     mov  result_number, input_number
     mov  result_weight, temp_weight
+    mov  result_address, current_address
     pop  {pc}
 
 /*** Function: store results ********************************************************************/
@@ -61,6 +62,8 @@ store_output:                                       @ Store Results in Output Va
     str  result_number, [temp]
     ldr  temp, =output_weight
     str  result_weight, [temp]
+    ldr  temp, =output_address
+    str  result_address, [temp]
     pop  {pc}
 
 /*** main Function ******************************************************************************/
@@ -75,9 +78,9 @@ _main:
         bl   get_weight                             @ Compute the Weight in temp_weight
         cmp  result_weight, temp_weight             @ Compare result_weight with temp_weight
         bllt update_results                         @ Update if temp_weight is greater
-        cmp current_address, end_address            @ Compare addresses
-        bne loop                                    @ Check if number reached end and continue
-    bl store_output                                 @ Store the results
+        cmp  current_address, end_address           @ Compare addresses
+        bne  loop                                   @ Check if number reached end and continue
+    bl   store_output                               @ Store the results
     .end
 
 /*** End ****************************************************************************************/
